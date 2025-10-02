ede94bb18f6c9076bfa214d04ca2966de8bc75ce
frontend-mahdi <mahdi.313.ab@gmail.com>
2025-10-02 16:55:56 +0330

    feat: Added some extra fields from model in serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -8,11 +8,13 @@ from ...models import Post,Category
 class PostSerializer(serializers.ModelSerializer):
     #1 content = serializers.ReadOnlyField()
     #2 content = serializers.CharField(read_only=True)
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
     class Meta:
         model = Post
         fields = '__all__'
-        # fields = ['id','title','content','status','created_date','published_date']
-        read_only_fields = ['content']
+        fields = ['id','title','content','status','snippet','relative_url ','created_date','published_date']
+        #3 read_only_fields = ['content']
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
