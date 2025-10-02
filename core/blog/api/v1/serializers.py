9528984b150cdfa4322fe958a25c9762da6fe35e
frontend-mahdi <mahdi.313.ab@gmail.com>
2025-10-02 17:02:44 +0330

    feat: Added SerializerMethodField absolute url

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -10,12 +10,16 @@ class PostSerializer(serializers.ModelSerializer):
     #2 content = serializers.CharField(read_only=True)
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
+    absolute_url =serializers.SerializerMethodField(method_name='get_abs_url')
     class Meta:
         model = Post
         fields = '__all__'
-        fields = ['id','title','content','status','snippet','relative_url ','created_date','published_date']
+        fields = ['id','title','content','status','snippet','relative_url','absolute_url','created_date','published_date']
         #3 read_only_fields = ['content']
-
+    def get_abs_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+    
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
